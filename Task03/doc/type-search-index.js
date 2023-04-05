bcd3ab8cbe4b58e3e6e3a9c466b735c3434d5ceb
Tumko Ivan <114734820+Ivan-blep@users.noreply.github.com>
2023-04-05 11:47:39 +0300

    Delete Task03/doc directory

--- Task03/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BallisticTrajectoryDemo"},{"p":"<Unnamed>","l":"BallisticTrajectoryF"},{"p":"<Unnamed>","l":"BallisticTrajectoryM"},{"p":"<Unnamed>","l":"BallisticTrajectoryTest"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
